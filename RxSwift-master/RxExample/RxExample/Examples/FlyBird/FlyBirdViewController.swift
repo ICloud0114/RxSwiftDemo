33e53981d5a36c05f1ba37d229fa1a14eb32531b
ICloud <zy@ZhengMacBook-Pro.local>
2017-12-22 17:59:24 +0800

    111

--- RxSwift-master/RxExample/RxExample/Examples/FlyBird/FlyBirdViewController.swift
+++ RxSwift-master/RxExample/RxExample/Examples/FlyBird/FlyBirdViewController.swift
@@ -7,18 +7,17 @@
 //
 
 import UIKit
-import CocoaAsyncSocket
 
 let filePath = NSHomeDirectory() + "/Documents/users.plist"
 
-class FlyBirdViewController: UIViewController,GCDAsyncSocketDelegate {
+class FlyBirdViewController: UIViewController {
 
     @IBOutlet weak var singleButton: UIButton!
     @IBOutlet weak var limitButton: UIButton!
     @IBOutlet weak var longTimeButton: UIButton!
     @IBOutlet weak var mainScrollView: UIScrollView!
     
-    var socket: GCDAsyncSocket?
+//    var socket: GCDAsyncSocket?
     var longTimer: Timer?
     var viewcontrollers:[UIViewController] = []
     override func viewDidLoad() {
@@ -29,64 +28,64 @@ class FlyBirdViewController: UIViewController,GCDAsyncSocketDelegate {
         navigationItem.rightBarButtonItem = saveItem
         
         // Do any additional setup after loading the view.
-        socket = GCDAsyncSocket(delegate: self, delegateQueue: DispatchQueue.main)
-        
-        do {
-            let _ = try socket?.connect(toHost: "121.15.200.120", onPort: 5577)
-//            longTimer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(longConnectToSocket), userInfo: nil, repeats: true)
-//            RunLoop.current.add(longTimer!, forMode: .commonModes)
-        } catch (_) {
-//            rxFatalError("RefCountDisposable decrement on release failed")
-        }
-
-        guard let _ = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) else {
-            let array:[[String]] = []
-            NSKeyedArchiver.archiveRootObject(array, toFile: filePath)
-            return
-        }
-        
-        NSKeyedArchiver.archiveRootObject([], toFile: filePath)
+//        socket = GCDAsyncSocket(delegate: self, delegateQueue: DispatchQueue.main)
+//
+//        do {
+//            let _ = try socket?.connect(toHost: "121.15.200.120", onPort: 5577)
+////            longTimer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(longConnectToSocket), userInfo: nil, repeats: true)
+////            RunLoop.current.add(longTimer!, forMode: .commonModes)
+//        } catch (_) {
+////            rxFatalError("RefCountDisposable decrement on release failed")
+//        }
+//
+//        guard let _ = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) else {
+//            let array:[[String]] = []
+//            NSKeyedArchiver.archiveRootObject(array, toFile: filePath)
+//            return
+//        }
+//
+//        NSKeyedArchiver.archiveRootObject([], toFile: filePath)
     }
     
-    func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
-       
-        sock.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
-        socket?.readData(withTimeout: -1, tag: 0)
-    }
-    
-    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
-        
-        let message = String(bytes: data, encoding: .utf8)
-//        let message = "FNText_456789_Open_201711150927"
-        print("receive message:" + message!)
-        if  message!.hasPrefix("FNText"){
-            
-            let subMessage: [String] = message!.components(separatedBy: "FNText").filter({$0.count > 0})
-            let result = subMessage.map({$0.components(separatedBy: "_").filter({$0.count > 0})
-            })
-
-            guard var history = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) as? [[String]] else {
-                return
-            }
-
-            history.append(contentsOf: result)
-
-            NSKeyedArchiver.archiveRootObject(history, toFile: filePath)
-        }
-        socket?.readData(withTimeout: -1, tag: 0)
-    }
-
-    func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
-        
-        print("tcp disconnect")
-        socket?.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
-    }
-    
-    @objc func longConnectToSocket(){
-
-        socket?.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
-
-    }
+//    func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
+//
+//        sock.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
+//        socket?.readData(withTimeout: -1, tag: 0)
+//    }
+//
+//    func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
+//
+//        let message = String(bytes: data, encoding: .utf8)
+////        let message = "FNText_456789_Open_201711150927"
+//        print("receive message:" + message!)
+//        if  message!.hasPrefix("FNText"){
+//
+//            let subMessage: [String] = message!.components(separatedBy: "FNText").filter({$0.count > 0})
+//            let result = subMessage.map({$0.components(separatedBy: "_").filter({$0.count > 0})
+//            })
+//
+//            guard var history = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) as? [[String]] else {
+//                return
+//            }
+//
+//            history.append(contentsOf: result)
+//
+//            NSKeyedArchiver.archiveRootObject(history, toFile: filePath)
+//        }
+//        socket?.readData(withTimeout: -1, tag: 0)
+//    }
+//
+//    func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
+//
+//        print("tcp disconnect")
+//        socket?.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
+//    }
+//
+//    @objc func longConnectToSocket(){
+//
+//        socket?.write("FNLinktest".data(using: .utf8)!, withTimeout: -1, tag: 0)
+//
+//    }
     @objc func pushToHistoryList(){
         let storyboard = UIStoryboard(name: "HistoryList", bundle: nil)
         
